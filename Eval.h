97d18c94f86571619fd282f96cb284b744c10a87
Igor Motyka <igor_motyka@mail.com>
2023-11-17 16:58:21 +0100

    Messy and unclean but cells now depend on one another

--- Eval.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <regex>
-
-#include "Base.h"
-#include "tinyexpr.h"
-
-bool evaluate(Cell*, const std::vector<Cell>&, const std::vector<Column>&);
--- ProgramState.cpp
+++ ProgramState.cpp
@@ -1,9 +1,9 @@
 #include "ProgramState.h"
 #include <string>
 
-Mode* ProgramState::getCurrentMode() 
+Mode* ProgramState::getCurrentMode()
 {
-	for (Mode* mode : _modes) 
+	for (Mode* mode : _modes)
 	{
 		if (mode->type == currentMode)
 			return mode;
@@ -67,18 +67,18 @@ Cell* ProgramState::getCellToThe(Cell* cell, Direction direction) {
 	int cellIndex = 0;
 	switch (direction)
 	{
-		case Left:
-			cellIndex = cell->index - 1;
-			break;
-		case Right:
-			cellIndex = cell->index + 1;
-			break;
-		case Up:
-			cellIndex = cell->index - numberOfColumns;
-			break;
-		case Down:
-			cellIndex = cell->index + numberOfColumns;
-			break;
+	case Left:
+		cellIndex = cell->index - 1;
+		break;
+	case Right:
+		cellIndex = cell->index + 1;
+		break;
+	case Up:
+		cellIndex = cell->index - numberOfColumns;
+		break;
+	case Down:
+		cellIndex = cell->index + numberOfColumns;
+		break;
 	}
 
 	if (cellIndex < 0 || cellIndex >= cells.size()) return NULL;
@@ -86,7 +86,7 @@ Cell* ProgramState::getCellToThe(Cell* cell, Direction direction) {
 	return &cells[cellIndex];
 }
 
-std::string ProgramState::getCellPosLabel(Cell* cell){
+std::string ProgramState::getCellPosLabel(Cell* cell) {
 	std::string colLabel = columns[cell->colIndex].content;
 	std::string rowLabel = rows[cell->rowIndex].content;
 
@@ -100,4 +100,91 @@ void ProgramState::switchMode(SDL_Renderer* renderer, ModeType modeType) {
 	Mode* mode = getCurrentMode();
 
 	mode->onSwitchTo(renderer, this);
+}
+
+bool ProgramState::evaluate(SDL_Renderer* renderer, Cell* cell) {
+
+	bool haveFormula = cell->formula.length() > 0;
+	bool haveExprInContent = cell->content.length() > 0 && cell->content[0] == '=';
+	bool doNotEval = !haveFormula && !haveExprInContent;
+
+	if (doNotEval) {
+		for (Cell* dependant_cell : cell->dependant_cells) {
+			if (evaluate(renderer, dependant_cell))
+				dependant_cell->showEvaluation(renderer, font, fontColor, cellPadding);
+		}
+
+		return false;
+	};
+
+	std::string expr;
+	if (haveExprInContent) {
+		expr = cell->content;
+		cell->formula = cell->content;
+	}
+	else
+		expr = cell->formula;
+
+	expr = expr.erase(0, 1); // Removes '='
+
+	std::regex labelsRgx(R"(([A-Z])+(\d)+)");
+	std::smatch match;
+
+	bool haveCellPosToReplace = true;
+
+	while (haveCellPosToReplace) {
+		std::string exprCopy = expr;
+		auto regexIterStart = std::sregex_iterator(exprCopy.begin(), exprCopy.end(), labelsRgx);
+		auto regexIterEnd = std::sregex_iterator();
+
+		haveCellPosToReplace = false;
+		for (std::sregex_iterator i = regexIterStart; i != regexIterEnd; ++i) {
+
+			haveCellPosToReplace = true;
+
+			std::smatch match = *i;
+			std::string cellPos = match[0];
+
+			//Note(Igor): this will not work on two and more char columns 
+			std::string col = cellPos.substr(0, 1);
+			int rowIndex = std::stoi(cellPos.substr(1)) - 1;
+
+			int colIndex = (int)col[0] - 65;
+			int cellIndex = (rowIndex * columns.size()) + colIndex;
+
+			//TODO(Igor): COPY
+			assert(cellIndex <= cells.size());
+
+			Cell* c = &cells.at(cellIndex);
+
+			//Note(Igor): side effect
+			if (c->index == cell->index) {
+				cell->Err = SelfReference;
+				return false;
+			}
+
+			c->dependant_cells.insert(cell);
+
+			std::regex r(cellPos);
+			expr = std::regex_replace(expr, r, c->content); //Info(Igor): slooooow
+		}
+	}
+
+	int err = 0;
+	double result = te_interp(expr.c_str(), &err);
+	cell->content = std::to_string(result);
+
+	for (Cell* dependant_cell : cell->dependant_cells) {
+		if (evaluate(renderer, dependant_cell))
+			dependant_cell->showEvaluation(renderer, font, fontColor, cellPadding);
+	}
+
+	if (err) {
+		cell->Err = NaN;
+		return false;
+	}
+	else {
+		cell->Err = Ok;
+		return true;
+	}
 }
